0c643f00349539990033aa0fdcee9283c8b1471e
bisqet <greenelephantt@gmail.com>
2024-08-20 00:15:32 +0300

    update test validResult

--- tests/_test.js
+++ tests/_test.js
@@ -2,41 +2,28 @@ import { expect, test } from "bun:test";
 import {generateImportMaps} from '../generateImportMaps.js';
 import * as path from "node:path";
 
+const baseValidResult = {
+  "imports": {
+    "lit": "../../node_modules/lit/index.js"
+  },
+  "scopes": {
+    "../../node_modules/": {
+      "@lit/reactive-element": "../../node_modules/@lit/reactive-element/reactive-element.js",
+      "lit-element/lit-element.js": "../../node_modules/lit-element/lit-element.js",
+      "lit-html": "../../node_modules/lit-html/lit-html.js",
+      "lit-html/is-server.js": "../../node_modules/lit-html/is-server.js"
+    }
+  }
+}
 test("Generate Import Maps for JS input", async () => {
-  const validResult = {
-    imports: {
-      lit: "./node_modules/lit/index.js",
-    },
-    scopes: {
-      "./node_modules/": {
-        "@lit/reactive-element": "./node_modules/@lit/reactive-element/reactive-element.js",
-        "lit-element/lit-element.js": "./node_modules/lit-element/lit-element.js",
-        "lit-html": "./node_modules/lit-html/lit-html.js",
-        "lit-html/is-server.js": "./node_modules/lit-html/is-server.js",
-      },
-    },
-  };
   const testResult = await generateImportMaps({input:path.resolve(import.meta.dir, './assets/test-element.js')});
-  console.log(validResult, testResult)
-  expect(testResult).toEqual(validResult);
+  console.log(baseValidResult, testResult)
+  expect(testResult).toEqual(baseValidResult);
 });
 test("Generate Import Maps for HTML input", async () => {
-  const validResult = {
-    imports: {
-      lit: "./node_modules/lit/index.js",
-    },
-    scopes: {
-      "./node_modules/": {
-        "@lit/reactive-element": "./node_modules/@lit/reactive-element/reactive-element.js",
-        "lit-element/lit-element.js": "./node_modules/lit-element/lit-element.js",
-        "lit-html": "./node_modules/lit-html/lit-html.js",
-        "lit-html/is-server.js": "./node_modules/lit-html/is-server.js",
-      },
-    },
-  };
   const testResult = await generateImportMaps({input:path.resolve(import.meta.dir, './assets/test.html'), writeToDisk:false});
-  console.log(validResult, testResult)
-  expect(testResult).toEqual(validResult);
+  console.log(baseValidResult, testResult)
+  expect(testResult).toEqual(baseValidResult);
 });
 test("Write generated maps", async () => {
   const validResult = await Bun.file(path.resolve(import.meta.dir, './assets/valid.html')).text();
